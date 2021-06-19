5688703bb2b4559538d4fbfd89646a43736e8f83
Luis Fernando Ushiña <guffenix@gmail.com>
2021-06-19 15:53:03 -0500

    inicio testing

---
+++ src/testing_jekins/Main.java
@@ -0,0 +1,42 @@
+package testing_jekins;
+
+public class Main {
+
+	public static void main(String[] args ) {
+		StringBuilder cat = new StringBuilder();
+		cat.append("000000O,oc;:;;0000OOx'oc;::.oddddddddddddddddddddddd").append("\n");
+		cat.append("000000O,o:;:,;OOOOOOO,oo;::.oddddddddddddddddddddddd").append("\n");
+		cat.append("000000O,o:;:,;0d:llOO,oo;::.odddl::ldddddddddddddddd").append("\n");
+		cat.append("000000O,o:;:,:0;xKxco,oo;::'cc:ldkd,dddddddddddddddd").append("\n");
+		cat.append("0000000,o:::;lO,ONNWWOlo;::lXKKKkd,ldddddddddddddddd").append("\n");
+		cat.append("000000O;l::l;ox;NMMMMOll;::cXXXXXl;xkxdddddddddddddd").append("\n");
+		cat.append("000000O;l:::.l:xWWWWWOll;::cKK000O'dxddddddddddddddd").append("\n");
+		cat.append("0000000;l:::.l;XNOkkkoll;::;xOk0O0;dOkxddddddddddddd").append("\n");
+		cat.append("0000OO0;l:;:.l;WMMMMMOlo;:::NMMMMW:oOOOOkddddddddddd").append("\n");
+		cat.append("00000OO,l:;:.ocKMMMMMOlo;:::NMMMMN:k000OOOOxdddddddd").append("\n");
+		cat.append("000000O,ol:l,dOckWMMM0lo;:::NMMMNcoOO000000OOkdddddd").append("\n");
+		cat.append("0000O00:do::.oOOdldkK0lo;:::NWKdlxO00000000000xddddd").append("\n");
+		cat.append("0000000;ol;:.oO00OOx;clo;::cXXlcOOO0000000000Oxddddd").append("\n");
+		cat.append("0000000;ol;:.oOOOO0ocOlo;::cXXO,k000000000000Oxddddd").append("\n");
+		cat.append("0000000;ol;:.oOO0OolWKlo;:::NWWx:O00000000000Okddddd").append("\n");
+		cat.append("000000O;ol;:.oOOOcoWMKlo;:::NMMWolOO000000000Okddddd").append("\n");
+		cat.append("0000000,ol;:.oOxckWMMKlo;::cNMMMWloO000000000OOddddd").append("\n");
+		cat.append("00000OO,oc;:.olcKMMMMKlo;::cWMMMMN:dOO0000000OOddddd").append("\n");
+		cat.append("00000OO,oc;:.,dWMMMMMKlo;::lMMMMMMX:xO0000000OOddddd").append("\n");
+		cat.append("00000OO;o:;:.lWMMMMMM0lo;::lMMMMMMMK,k00000000Oxdddd").append("\n");
+		cat.append("0000000,o:;::XMMMMMMMOlo;::oMMMMMMMMoc0000000OOxdddd").append("\n");
+		cat.append("000000O,o:;;kWMMMMMMMOlo;::oMMMMMMMMX;O00000OOOxdddd").append("\n");
+		cat.append("000000O,o:;;KMMMMMMMMOlo;::oMMMMMMMMW:kO000000Oxdddd").append("\n");
+		cat.append("OOOOOOO,o:;;KMMWMMMMMOlo;::lMMMMMMMMW:kOOOOOOOOxdddd").append("\n");
+		cat.append("lllllll'o:;;dWWNKMMMMOoo;::cWMMXXMMMK'cccc::::::;::;").append("\n");
+		cat.append("ooooooo;:,','OMM0KMMMOoo;:::NMMkNMWK;;::::::::::::::").append("\n");
+		cat.append("oooooooooolll;xWNkWMMOol;:::NMNkWNO;,:::::;:cccccc:;").append("\n");
+		cat.append("oooooooooooooo;;OkkWMxoc;:::NM0OOx0X0OkkkOKXXXXXXXXX").append("\n");
+		cat.append("ooooooooooooooo:::,xWxd:;:::NWxOd:,cldxxkxkkkxxxxddo").append("\n");
+		cat.append("oooooooooooooooooo:,o:d;;:::Od;,:clcc:;;;;;;;;;;;:::").append("\n");
+		cat.append("ooooooooooooooooooool,:;::;.,,;:::::::::::::::::::::").append("\n");
+		cat.append("oooooooooooooooooooooo;....,::::::::::::::::::::::::").append("\n");
+		
+		System.out.println(cat.toString());
+	}
+}
